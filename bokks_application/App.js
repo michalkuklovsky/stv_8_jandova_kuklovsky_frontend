c29fa70f5fed7932f93041b58cec8e155f074ce3
michalkuklovsky <kuklovsky.michal@gmail.com>
2022-04-11 13:22:34 +0200

    json on homepage

--- bokks_application/App.js
+++ bokks_application/App.js
@@ -1,110 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  Text,
+  FlatList,
+  StyleSheet,
+  SafeAreaView,
+  ActivityIndicator,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const homeURL = 'http://192.168.56.1:8000/';
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const [isLoading, setLoading] = useState(true);
+  const [books, setBooks] = useState([]);
+  const [events, setEvents] = useState([]);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    fetch(homeURL, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(json => {
+        setBooks(json.books);
+        setEvents(json.events);
+      })
+      .catch(error => alert(error))
+      .finally(() => setLoading(false));
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            HELLO
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <View>
+          <Text>Books</Text>
+          <FlatList
+            data={books}
+            keyExtractor={({id}, index) => id}
+            renderItem={({item}, index) => (
+              <Text>
+                Title: {item.title}
+                <Text> </Text>
+                ISBN: {item.isbn}
+              </Text>
+            )}
+          />
+          <Text>Events</Text>
+          <FlatList
+            data={events}
+            keyExtractor={({id}, index) => id}
+            renderItem={({item}, index) => (
+              <Text>
+                Title: {item.name}
+                <Text> </Text>
+                User ID: {item.user_id}
+              </Text>
+            )}
+          />
         </View>
-      </ScrollView>
+      )}
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
