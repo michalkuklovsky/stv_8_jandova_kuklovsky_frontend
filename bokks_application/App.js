13da7411e98f701ff3997efd184354fb57d49271
michalkuklovsky <kuklovsky.michal@gmail.com>
2022-04-12 15:54:25 +0200

    refactoring

--- bokks_application/App.js
+++ bokks_application/App.js
@@ -1,43 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-
-import LoginScreen from './screens/LoginScreen';
-import CallScreen from './screens/CallScreen';
-import Genres from './screens/GenresCategories';
-import Homepage from './screens/Homepage';
-
-const Stack = createNativeStackNavigator();
+import Navigator from './app/navigator/navigator';
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Homepage"
-          component={Homepage}
-          options={{headerShown: false}}
-        />
-
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen name="Call" component={CallScreen} />
-        <Stack.Screen
-          name="Genres"
-          component={Genres}
-          options={{headerShown: false}}
-        />
-
-        {/*<Stack.Screen*/}
-        {/*    name="DrawerNavigationRoutes"*/}
-        {/*    component={DrawerNavigationRoutes}*/}
-        {/*    // Hiding header for Navigation Drawer*/}
-        {/*    options={{headerShown: false}}*/}
-        {/*/>*/}
-      </Stack.Navigator>
+      <Navigator />
     </NavigationContainer>
   );
 };
---
+++ bokks_application/app/navigator/navigator.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/home/HomePage';
+import LoginScreen from '../screens/auth/LoginScreen';
+import Genres from '../screens/books/GenresCategories';
+import CallScreen from '../screens/events/CallScreen';
+
+const Navigator = () => {
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <Stack.Navigator
+      initialRouteName="Homepage"
+      screenOptions={{
+        headerShown: false,
+        headerMode: 'none',
+        statusbar: 'none',
+      }}>
+      <Stack.Screen
+        name="Homepage"
+        component={HomeScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen name="Call" component={CallScreen} />
+      <Stack.Screen
+        name="Genres"
+        component={Genres}
+        options={{headerShown: false}}
+      />
+
+      {/*<Stack.Screen*/}
+      {/*    name="DrawerNavigationRoutes"*/}
+      {/*    component={DrawerNavigationRoutes}*/}
+      {/*    // Hiding header for Navigation Drawer*/}
+      {/*    options={{headerShown: false}}*/}
+      {/*/>*/}
+    </Stack.Navigator>
+  );
+};
+
+export default Navigator;
