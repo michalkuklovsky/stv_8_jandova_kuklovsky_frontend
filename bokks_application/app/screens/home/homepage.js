b77102aece82884c5f0a4332df351748a3d80ea4
michalkuklovsky <kuklovsky.michal@gmail.com>
2022-04-17 22:05:35 +0200

    merge

--- bokks_application/app/screens/home/homepage.js
+++ bokks_application/app/screens/home/homepage.js
@@ -38,170 +38,162 @@ const HomeScreen = ({navigation}) => {
         <ActivityIndicator />
       ) : (
         <View>
-          <AppHeader
-            title={'BOKKS'}
-            titleOnPress={() => navigation.navigate('Home')}
-            headerBg={'blue'}
-            iconColor={'white'}
-            titleAlight={'left'}
-            right={'shopping-cart'}
-            onRightPress={() => navigation.navigate('Cart')}
-          />
-          <Text>Books</Text>
-          <FlatList
-            data={books}
-            keyExtractor={({id}, index) => id}
-            renderItem={({item}, index) => (
-              <Text>
-                Title: {item.title}
-                <Text> </Text>
-                ISBN: {item.isbn}
-              </Text>
-            )}
-          />
-          {/* <Text>Events</Text>
-          <FlatList
-            data={events}
-            keyExtractor={({id}, index) => id}
-            renderItem={({item}, index) => (
-              <Text>
-                Title: {item.name}
-                <Text> </Text>
-                User ID: {item.user_id}
-              </Text>
-            )}
-          /> */}
-          <Button
-            title="Login"
-            color="white"
-            onPress={() =>
-              navigation.navigate('NavBar', {screen: 'Login'})
-            }
-            style={styles.btn}
-          >{'Login'}
-          </Button>
+          <View>
+            <AppHeader
+              title={'BOKKS'}
+              titleOnPress={() => navigation.navigate('Home')}
+              headerBg={'blue'}
+              iconColor={'white'}
+              titleAlight={'left'}
+              right={'shopping-cart'}
+              onRightPress={() => navigation.navigate('Cart')}
+            />
+          </View>
+          <View style={StyleSheet.create({padding: 20})}>
+            <Text style={styles.mainText}>Books</Text>
+            <FlatList
+              data={books}
+              keyExtractor={({id}, index) => id}
+              renderItem={({item}, index) => (
+                <Text style={styles.mainText}>
+                  Title: {item.title}
+                  <Text> </Text>
+                  ISBN: {item.isbn}
+                </Text>
+              )}
+            />
+            {/* <Text style={styles.mainText}>Events</Text>
+            <FlatList
+              data={events}
+              keyExtractor={({id}, index) => id}
+              renderItem={({item}, index) => (
+                <Text style={styles.mainText}>
+                  Title: {item.name}
+                  <Text> </Text>
+                  User ID: {item.user_id}
+                </Text>
+              )}
+            /> */}
+            <Button
+              title="Login"
+              color="white"
+              onPress={() =>
+                navigation.navigate('NavBar', {screen: 'Login'})
+              }
+              style={styles.btn}
+            >{'Login'}
+            </Button>
 
-          {/* <Button
-            title="BooksDetail"
-            color="white"
-            onPress={() =>
-              navigation.navigate('BooksDetail')
-            }
-            style={styles.btn}
-          >{'BooksDetail'}
-          </Button> */}
+            {/* <Button
+              title="BooksDetail"
+              color="white"
+              onPress={() =>
+                navigation.navigate('BooksDetail')
+              }
+              style={styles.btn}
+            >{'BooksDetail'}
+            </Button> */}
 
-          <Button
-            title="BooksList"
-            color="white"
-            onPress={() =>
-              navigation.navigate('BooksList')
-            }
-            style={styles.btn}
-          >{'BooksList'}
-          </Button>
+            <Button
+              title="BooksList"
+              color="white"
+              onPress={() =>
+                navigation.navigate('BooksList')
+              }
+              style={styles.btn}
+            >{'BooksList'}
+            </Button>
 
-          <Button
-            title="GenresCategories"
-            color="white"
-            onPress={() =>
-              navigation.navigate('NavBar', {screen: 'Genres'})
-            }
-            style={styles.btn}
-          >{'GenresCategories'}
-          </Button>
+            <Button
+              title="GenresCategories"
+              color="white"
+              onPress={() =>
+                navigation.navigate('NavBar', {screen: 'Genres'})
+              }
+              style={styles.btn}
+            >{'GenresCategories'}
+            </Button>
 
-          <Button
-            title="GenreResults"
-            color="white"
-            onPress={() =>
-              navigation.navigate('GenreResults')
-            }
-            style={styles.btn}
-          >{'GenreResults'}
-          </Button>
+            <Button
+              title="GenreResults"
+              color="white"
+              onPress={() =>
+                navigation.navigate('GenreResults')
+              }
+              style={styles.btn}
+            >{'GenreResults'}
+            </Button>
 
-          <Button
-            title="SeachScreen"
-            color="white"
-            onPress={() =>
-              navigation.navigate('SearchScreen')
-            }
-            style={styles.btn}
-          >{'SeachScreen'}
-          </Button>
+            <Button
+              title="SeachScreen"
+              color="white"
+              onPress={() =>
+                navigation.navigate('SearchScreen')
+              }
+              style={styles.btn}
+            >{'SeachScreen'}
+            </Button>
 
-          {/* <Button
-            title="Cart"
-            color="white"
-            // onPress={() => navigation.navigate('Cart')}
-            style={styles.btn}
-          >{'Cart'}
-          </Button> */}
+            {/* <Button
+              title="Cart"
+              color="white"
+              // onPress={() => navigation.navigate('Cart')}
+              style={styles.btn}
+            >{'Cart'}
+            </Button> */}
 
-          <Button
-            title="CallScreen"
-            color="white"
-            onPress={() =>
-              navigation.navigate('Call')
-            }
-            style={styles.btn}
-          >{'CallScreen'}
-          </Button>
+            <Button
+              title="CallScreen"
+              color="white"
+              onPress={() =>
+                navigation.navigate('Call')
+              }
+              style={styles.btn}
+            >{'CallScreen'}
+            </Button>
 
-          {/* <Button
-            title="EventsDetails"
-            color="white"
-            onPress={() => navigation.navigate('EventsDetails')}
-            style={styles.btn}
-          >{'EventsDetails'}
-          </Button> */}
+            {/* <Button
+              title="EventsDetails"
+              color="white"
+              onPress={() => navigation.navigate('EventsDetails')}
+              style={styles.btn}
+            >{'EventsDetails'}
+            </Button> */}
 
-          {/* <Button
-            title="Events"
-            color="white"
-            onPress={() => navigation.navigate('Events')}
-            style={styles.btn}
-          >{'Events'}
-          </Button> */}
+            {/* <Button
+              title="Events"
+              color="white"
+              onPress={() => navigation.navigate('Events')}
+              style={styles.btn}
+            >{'Events'}
+            </Button> */}
 
-          <Button
-            title="Homepage"
-            color="white"
-            onPress={() =>
-              navigation.navigate('NavBar', {screen: 'Homepage'})
-            }
-            style={styles.btn}
-          >{'Homepage'}
-          </Button>
+            <Button
+              title="Homepage"
+              color="white"
+              onPress={() =>
+                navigation.navigate('NavBar', {screen: 'Homepage'})
+              }
+              style={styles.btn}
+            >{'Homepage'}
+            </Button>
 
-          {/* <Button
-            title="SearchResults"
-            color="white"
-            onPress={() =>
-              navigation.navigate('SearchResults')
-            }
-            style={styles.btn}
-          >{'SearchResults'}
-          </Button> */}
-          {/* <TestButtons /> */}
+            {/* <Button
+              title="SearchResults"
+              color="white"
+              onPress={() =>
+                navigation.navigate('SearchResults')
+              }
+              style={styles.btn}
+            >{'SearchResults'}
+            </Button> */}
+            {/* <TestButtons /> */}
+          </View>
         </View>
-      )}
+    )}
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  btn: {
-    backgroundColor: '#006BFF',
-    color: 'white',
-    height: 60,
-    borderRadius: 25,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    fontSize: 12,
-  },
-});
 
 const TestButtons = ({navigation}) => {
   return (
@@ -325,6 +317,22 @@ const TestButtons = ({navigation}) => {
       </Button>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  btn: {
+    backgroundColor: '#006BFF',
+    color: 'white',
+    height: 60,
+    borderRadius: 25,
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    fontSize: 18,
+  },
+  mainText: {
+    color: 'black',
+  },
+});
+
 
 export default HomeScreen;
