8261153b3f9c31d1e85d5e36137b18f110179dc3
michalkuklovsky <kuklovsky.michal@gmail.com>
2022-04-20 11:34:17 +0200

    constants added

---
+++ bokks_application/app/Constants.js
@@ -0,0 +1,9 @@
+// export const appURL = 'http://147.175.182.110:8000/';
+export const appURL = 'http://10.62.44.90:8000/';
+// export const appURL = 'http://192.168.1.20:8000/';
+// export const appURL = 'http://192.168.1.10:8000/';
+
+// npm install react-native-safe-area-context
+// npm install @rneui/themed @rneui/base --legacy-peer-deps
+// npm install react-native-elements --legacy-peer-deps
+
