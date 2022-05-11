3ec8319c01ec3e54ce3c0bd511eb526686c8a91f
alexandra-j <alexandra-j@users.github.com>
2022-05-11 20:34:20 +0200

    nadherny offline screen

--- bokks_application/app/components/OfflineScreen.js
+++ bokks_application/app/components/OfflineScreen.js
@@ -5,7 +5,7 @@ import {
     SafeAreaView,
 } from 'react-native';
 import {Title} from 'react-native-paper';
-import { ScreenHeader } from '../components/Headers';
+import { ScreenHeader } from './Headers';
 
 const OfflineScreen = ({navigation, route}) => {
 
@@ -15,9 +15,8 @@ const OfflineScreen = ({navigation, route}) => {
           <View>
             <ScreenHeader navigation={navigation} />
           </View>
-          <View style={{padding: 20}}>
-
-            <Title style={styles.mainText}>Offline</Title>
+          <View style={styles.infoContainer}>
+            <Title style={styles.mainText}> You are offline! </Title>
 
           </View>
         </View>
@@ -26,61 +25,18 @@ const OfflineScreen = ({navigation, route}) => {
 };
 
 const styles = StyleSheet.create({
-  btn: {
-    backgroundColor: '#006BFF',
-    color: 'white',
-    height: 60,
-    borderRadius: 25,
-    alignItems: 'stretch',
-    justifyContent: 'center',
-    fontSize: 18,
-  },
   mainText: {
     marginTop: 5,
     color: 'black',
   },
-  card: {
-    flexDirection: "row",
-    height: 130,
-    width: "90%",
+  infoContainer: {
+    height: 400,
+    width: '100%',
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 10,
-    shadowOpacity: 0.25,
-    marginTop: 10,
-    marginBottom: 30,
-    alignSelf: "center"
-  },
-  infoContainer: {
-    flex: 1,
     flexDirection: "column",
-    alignSelf: "flex-end",
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
-  imageContainer: {
-    flex: 1,
-    position: "absolute",
-    flexDirection: "column",
-    alignSelf: "flex-start",
-    padding: 10,
-    height: "100%",
-  },
-  imgTitle: {
-    fontSize: 18,
-    padding: 4,
-    paddingRight: 14,
-    color: "black",
-    alignSelf: "flex-start"
-  },
-  logo: {
-    width: 100,
-    height: 110,
-    borderRadius: 10,
-  },
-  imgSub: {
-    paddingLeft: 6,
-      color: "black"
+    // alignSelf: "center",
+    paddingTop: 50,
   }
 });
 
